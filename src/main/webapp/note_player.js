ee8dcbf954d511ea6e37b441f43a122689c602e6
bgandhi <bgandhi@yahoo-inc.com>
2015-10-09 06:34:26 -0700

    Music code

--- src/main/webapp/note_player.js
+++ src/main/webapp/note_player.js
@@ -3,7 +3,7 @@ function playArohanam() {
         osc = audioContext.createOscillator();
         // 1 second divided by number of beats per second times number of beats (length of a note)
         osc.type = 'sine';
-        osc.frequency.value = noteFrequencies[notes[dvPattern1[i]]];
+        osc.frequency.value = noteFrequencies[notes[dvPattern1[i] + line]];
         osc.start(t);
         osc.stop(t + playlength);
         t += playlength;
