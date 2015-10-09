c5e77c831e7213acf96f5ff3b3e440b9410b956e
bgandhi <bgandhi@yahoo-inc.com>
2015-10-09 07:13:23 -0700

    Music code

--- src/main/webapp/note_printer.js
+++ src/main/webapp/note_printer.js
@@ -2,15 +2,16 @@ var x = 0, y = 0;
 
 var j = 0;
 
-function printArohanam() {
+function printNotes() {
     x += cw / vsplit;
-    y = noteRelativeLocations[notes[dvPattern1[j] + line]] * ch / hsplit;
+    var noteNum = isAvarohanam ? 9 - dvPattern1[j] : dvPattern1[j];
+    y = noteRelativeLocations[notes[noteNum + line]] * ch / hsplit;
     var newX = x - cw / vsplit / 2 - 5;
     var newY = y - ch / hsplit / 2 + 5;
-    ctx.fillText(notes[dvPattern1[j] + line], newX, newY);
+    ctx.fillText(notes[noteNum + line], newX, newY);
     j++;
     if (j < dvPattern1.length) {
-        setTimeout(printArohanam, playlength * 1000);
+        setTimeout(printNotes, playlength * 1000);
     } else {
         x = 0;
         y = 0;
