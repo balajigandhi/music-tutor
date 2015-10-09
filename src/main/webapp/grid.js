de1ab1081371691092c38de9c2663059f63e87d0
bgandhi <bgandhi@yahoo-inc.com>
2015-10-09 05:06:16 -0700

    Music code

---
+++ src/main/webapp/grid.js
@@ -0,0 +1,18 @@
+function drawGrid(myCanvas) {
+    var cw = myCanvas.width;
+    var ch = myCanvas.height;
+    var vsplit = 16;
+    var hsplit = 8;
+
+    for (var x = 0; x <= cw; x += cw / vsplit) {
+        ctx.strokeStyle = 'lightgrey';
+        ctx.rect(x, 0, cw / vsplit, ch);
+        ctx.stroke();
+    }
+
+    for (var y = 0; y <= ch; y += ch / hsplit) {
+        ctx.strokeStyle = 'lightgrey';
+        ctx.rect(0, y, cw, ch / hsplit);
+        ctx.stroke();
+    }
+}
