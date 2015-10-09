95819b2e765cc2ad54b90bd8d1a3a2cba0b87111
bgandhi <bgandhi@yahoo-inc.com>
2015-10-09 05:25:10 -0700

    Music code

--- src/main/webapp/note_printer.js
+++ src/main/webapp/note_printer.js
@@ -2,12 +2,12 @@ var x = 0, y = 0;
 
 var j = 0;
 
-function printArohanam(ctx, playlength) {
+function printArohanam() {
     x += 50;
     y = noteRelativeLocations[dvPattern[j]] * 50;
     ctx.fillText(dvPattern[j], x, y);
     j++;
     if (j < dvPattern.length) {
-        setTimeout(printArohanam(ctx, playlength), playlength * 1000);
+        setTimeout(printArohanam, playlength * 1000);
     }
 }
